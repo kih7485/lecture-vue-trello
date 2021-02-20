9ffb0de7eb0d32185d35513bb786cdbf0ee6fc9f
inhanKang <korea4127@gmail.com>
2021-02-21 01:46:50 +0900

    네비게이션 가드

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,8 +16,30 @@
 </template>
 
 <script>
+import {board} from '../api'
 export default {
-    
+    data(){
+        return{
+            loading: false,
+            boards: '',
+            error: ''
+        }
+    },
+    created(){
+        this.fetchData()
+    },
+    methods: {
+        fetchData(){
+            this.loading = true;
+            board.fetch()
+            .then(data => {
+                this.boards = data;
+            })
+            .finally( () =>{
+                this.loading = false;
+            })
+        }
+    }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,16 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) =>{
+  const isAuth = localStorage.getItem('token');
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home , beforeEnter: requireAuth},
     { path: '/login', component: Login },
     { path: '/b/:bid', component: Board , children: [
         {path: 'c/:cid', component: Card}
