f5a8ea2489dfa5c3b25730469c713b87777a9595
inhanKang <korea4127@gmail.com>
2021-02-21 16:10:37 +0900

    로그인

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,48 +1,46 @@
 <template>
+  <div>
+    Home
     <div>
-        Home
-        <div>
-            Board List:
-            <ul>
-                <li>
-                    <router-link to="b/1">Board 1 </router-link>
-                </li>
-                <li>
-                    <router-link to="b/2">Board 2 </router-link>
-                </li>
-            </ul>
+      Board List: 
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <div v-for="b in boards" :key=b.id>
+          <pre>{{b}}</pre>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import {board} from '../api'
+
 export default {
-    data(){
-        return{
-            loading: false,
-            boards: '',
-            error: ''
-        }
-    },
-    created(){
-        this.fetchData()
-    },
-    methods: {
-        fetchData(){
-            this.loading = true;
-            board.fetch()
-            .then(data => {
-                this.boards = data;
-            })
-            .finally( () =>{
-                this.loading = false;
-            })
-        }
+  data() {
+    return {
+      loading: false,
+      boards: '',
+      error: ''
     }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      board.fetch()
+        .then(data => {
+          this.boards = data
+        })
+        .finally(_=> {
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
