064c50a47c9b507685351a9b18375d719d3257de
inhan <inhan@inhanui-MacBookPro.local>
2021-03-14 13:30:32 +0900

    drag

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,57 +1,81 @@
 <template>
-    <div>
-       <div class="board-wrapper">
-           <div class="board">
-               <div class="board-header">
-                   <span class="board-title">
-                       {{board.title}}
-                   </span>
-               </div>
-               <div class="list-section-wrapper">
-                   <div class="list-section">
-                       <div class="list-wrapper" v-for="list in board.lists" :key="list.pos">
-                           <List :data="list" />
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-       <router-view></router-view>
+  <div>
+    <div class="board-wrapper">
+      <div class="board">
+        <div class="board-header">
+          <span class="board-title">{{board.title}}</span>
+        </div>
+        <div class="list-section-wrapper">
+          <div class="list-section">
+            <div class="list-wrapper" v-for="list in board.lists" :key="list.pos">
+                <List :data="list" />
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
+     <router-view></router-view>
+  </div>
 </template>
 
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List.vue'
-
+import dragger from '../utils/dragger'
 export default {
-    components: {
-        List
-    },
-    data() {
-        return{
-            bid:0,
-            loading: true
-        }
-    },
-    computed: {
-        ...mapState({
-            board: 'board'
-        })
-    },
-    created(){
-        this.fetchData()
-    },
-    methods: {
-        ...mapActions([
-            'FETCH_BOARD'
-        ]),
-        fetchData(){
-            this.loading = true
-            this.FETCH_BOARD({id: this.$route.params.bid})
-                .then( () => this.loading = false)
-        }
+  components: { List },
+  data() {
+    return {
+      bid: 0,
+      loading: false,
+      cDragger: null
     }
+  },
+  computed: {
+    ...mapState({
+      board: 'board'
+    })
+  },
+  created() {
+    this.fetchData()
+  },
+  updated() {
+    this.setCardDragabble()
+  },
+  methods: {
+    ...mapActions([
+      'FETCH_BOARD',
+      'UPDATE_CARD'
+    ]),
+    fetchData() {
+      this.loading = true
+      this.FETCH_BOARD({id: this.$route.params.bid})
+        .then(() => this.loading = false)
+    },
+    setCardDragabble() {
+      if (this.cDragger) this.cDragger.destroy()
+    
+      this.cDragger = dragger.init(Array.from(this.$el.querySelectorAll('.card-list')))
+      this.cDragger.on('drop', (el, wrapper, target, silblings) => {
+        const targetCard = {
+          id: el.dataset.cardId * 1, 
+          listId: wrapper.dataset.listId * 1,
+          pos: 65535,
+        }
+        const {prev, next} = dragger.sibling({
+          el, 
+          wrapper, 
+          candidates: Array.from(wrapper.querySelectorAll('.card-item')), 
+          type: 'card'
+        })
+        
+        if (!prev && next) targetCard.pos = next.pos / 2
+        else if (!next && prev) targetCard.pos = prev.pos * 2
+        else if (next && prev) targetCard.pos = (prev.pos + next.pos) / 2
+        this.UPDATE_CARD(targetCard)
+      })
+    }
+  }
 }
 </script>
 
